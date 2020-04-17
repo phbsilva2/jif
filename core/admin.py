421ea072857dd2852c41fb3c648b2c9da80e7904
Paulo Borges <phbsilva@gmail.com>
2020-04-17 19:25:31 -0300

    Criado o cadastro de Unidade Organizacional
    Após a criação do modelo em models.py e o registro em admin.py foi gerado o arquivo para atualização do banco de dados com os seguintes comandos:
    $python manage.py makemigrations
    $python manage.py migrate

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import UnidadeOrganizacional
+
+
+@admin.register(UnidadeOrganizacional)
+class UnidadeOrganizacionalAdmin(admin.ModelAdmin):
+    list_filter = ('nome',)
+
--- core/models.py
+++ core/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+class UnidadeOrganizacional(models.Model):
+    nome = models.CharField(max_length=100)
+
+    class Meta:
+        verbose_name = 'Unidade Organizacional'
+        verbose_name_plural = 'Unidades Organizacionais'
+
+    def __str__(self):
+        return self.nome
+
