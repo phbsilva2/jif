317711253adb147d893f4d64b3adc6aef9506c13
Paulo Borges <phbsilva@gmail.com>
2020-04-18 09:55:42 -0300

    Criada  constraint para o campo nome em Unidade Organizacional
    O campo nome deve ser único.
    
    Também foi criada a documentação Docstring referenciado o texto do documento de caso de uso.

--- core/models.py
+++ core/models.py
@@ -1,9 +1,19 @@
 from django.db import models
 
 class UnidadeOrganizacional(models.Model):
+    """
+    UC02 – Manter Unidades Organizacionais:
+        FA01 - Incluir Unidade Organizacional:
+            2. O sistema apresenta os campos para entrada dos dados:
+                - Nome (Obrigatório / Não se repete)
+    """
+
     nome = models.CharField(max_length=100)
 
     class Meta:
+        constraints = [
+            models.UniqueConstraint(fields=['nome'], name='unique_unidadeorganizacional_nome')
+        ]
         verbose_name = 'Unidade Organizacional'
         verbose_name_plural = 'Unidades Organizacionais'
 
