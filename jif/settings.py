0553491af68e1043dbf802838e20ba243ff4ff2d
Paulo Borges <phbsilva@gmail.com>
2020-04-18 12:22:25 -0300

    Alterado o leiaute do django admin
    O leiaute o django admin foi alterado para o Django AdminLTE2.

--- jif/settings.py
+++ jif/settings.py
@@ -33,6 +33,8 @@ ALLOWED_HOSTS = ['*']
 INSTALLED_APPS = [
     'core',
     'bootstrap4',
+    'django_adminlte',
+    'django_adminlte_theme',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
